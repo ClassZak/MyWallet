dc6fac273fe53188f55b2358bf1c823d0b4eace3
Захар Голуб <pahargolub2@gmail.com>
2024-12-03 22:28:29 +0300

    home update

--- src/main/java/com/example/mywallet/Global.kt
+++ src/main/java/com/example/mywallet/Global.kt
@@ -1,6 +1,42 @@
 package com.example.mywallet
 
-abstract class Global {
-    lateinit var UserName : String
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.annotation.UiThread
+import androidx.core.R
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
 
+class Global {
+    public var UserName : String="Пользователь"
+
+    object globalInstance {
+        var global:Global = Global()
+    }
+
+
+    fun loadSettings(activity:Activity):Unit{
+        val settingsFileName="Settings.txt"
+        val file = File(activity.getExternalFilesDir(null),settingsFileName)
+
+        if (!file.exists()) {
+            try {
+                file.writeText(UserName)
+            } catch (e: Exception) {
+                activity.runOnUiThread {
+                    Toast.makeText(activity, e.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+        } else
+            Global.globalInstance.global.UserName = file.readText()
+    }
+
+    fun loadData(activity :Activity){
+
+
+        loadSettings(activity)
+    }
 }
--- src/main/java/com/example/mywallet/SplashScreenActivity.kt
+++ src/main/java/com/example/mywallet/SplashScreenActivity.kt
@@ -1,14 +1,20 @@
 package com.example.mywallet
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.ScaleAnimation
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import ru.example.mywallet.R
+import java.io.File
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +26,48 @@ class SplashScreenActivity : AppCompatActivity() {
             insets
         }
 
-        Thread(kotlinx.coroutines.Runnable {
-            startMainAnimation()
+        Thread(Runnable{
+            Thread.sleep(1000)
+            scaleView(findViewById(R.id.walletImage),1.0f,0.5f,1.0f,0.5f)
+            Thread.sleep(1000)
+            scaleView(findViewById(R.id.walletImage),0.5f,1.0f,0.5f,1.0f)
+            Thread.sleep(1000)
+            scaleView(findViewById(R.id.walletImage),1.0f,0.75f,1.0f,0.75f)
+            Thread.sleep(1000)
+            scaleView(findViewById(R.id.walletImage),0.75f,1.0f,0.75f,1.0f)
+
+
+            Global.globalInstance.global.loadData(this)
+            runOnUiThread{
+                showHelloMessage()
+            }
+
+            goToMainActivity()
         }).start()
     }
 
-    fun startMainAnimation(){
-        val imageViewRef:ImageView=findViewById(R.id.walletImage)
-        runOnUiThread {
-            val scaleXUp = ObjectAnimator.ofFloat(imageViewRef, "scaleX", 1f, 1.5f)
-            val scaleYUp = ObjectAnimator.ofFloat(imageViewRef, "scaleY", 1f, 1.5f)
+    fun showHelloMessage(){
+        val textViewRef=
+        findViewById<TextView>(R.id.helloText)
+        textViewRef.setText(resources.getText(R.string.helloMessage).toString()+Global.globalInstance.global.UserName)
+        textViewRef.visibility=View.VISIBLE
+    }
 
-            // Анимация уменьшения
-            val scaleXDown = ObjectAnimator.ofFloat(imageViewRef, "scaleX", 1.5f, 1f)
-            val scaleYDown = ObjectAnimator.ofFloat(imageViewRef, "scaleY", 1.5f, 1f)
-
-            // Создаем набор анимаций
-            val animatorSet = AnimatorSet()
-            animatorSet.play(scaleXUp).with(scaleYUp).after(scaleYDown).with(scaleXDown)
-            animatorSet.duration = 2000 // Установите продолжительность анимации
-            animatorSet.start()
-        }
-        Thread.sleep(5000)
+    fun goToMainActivity(){
+        Thread.sleep(2000)
         startActivity(Intent(this,MainActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+    }
+
+
+    fun scaleView(v: View, startXScale: Float, endXScale: Float,startYScale: Float, endYScale: Float) {
+        val anim: Animation = ScaleAnimation(
+            startXScale, endXScale,  // Start and end values for the X axis scaling
+            startYScale, endYScale,  // Start and end values for the Y axis scaling
+            Animation.RELATIVE_TO_SELF, 0.5f,  // Pivot point of X scaling
+            Animation.RELATIVE_TO_SELF, 0.5f
+        ) // Pivot point of Y scaling
+        anim.fillAfter = false // Needed to keep the result of the animation
+        anim.duration = 1000
+        v.startAnimation(anim)
     }
 }
